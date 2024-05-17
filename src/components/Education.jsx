c06d77a14e13f7551f9b7ee782a207f46b62f38a
wenhaolu <wenhaolu.kevin@gmail.com>
2024-05-17 12:23:07 -0400

    update imgs

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -29,7 +29,7 @@ export default function Education() {
           <div className="educationDate">{education.date}</div>
         </div>
       ))}
-
+    
       {/* courseContainer */}
       <div className="courseContainer">
         <div className="courseCard">
